ef9451fa2bd6efeed1aaa49847da551fc441aa43
allen_support <allen.support@webfactories.biz>
2016-04-18 11:26:11 +0800

    add template for pages and menu

--- assets/js/creative.js
+++ assets/js/creative.js
@@ -48,6 +48,24 @@
 
     new WOW().init();
 
+     function stripTrailingSlash(str) {
+    if(str.substr(-1) == '/') {
+      return str.substr(0, str.length - 1);
+    }
+    return str;
+  }
+
+  var url = window.location.pathname;
+  var activePage = stripTrailingSlash(url);
+
+  $('.nav li a').each(function(){
+    var currentPage = stripTrailingSlash($(this).attr('href'));
+
+    if (activePage == currentPage) {
+      $(this, 'a').addClass('active');
+    }
+  });
+
 
 
 })(jQuery); // End of use strict
